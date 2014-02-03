6a973c396956e6a29d1aeb88e7ebc3bf8bdd8e35
Thorsten Lorenz <thlorenz@gmx.de>
2014-02-03 13:20:12 -0500

    reorganized code to decouple from UI editors

--- main.js
+++ main.js
@@ -1,47 +1,29 @@
 'use strict';
 
-var util = require('./lib/util');
-
-var marked = require('marked');
-var createMdEditor = require('./lib/markdown-editor/');
-var createEditor = require('./lib/editor/');
-
-var debug = require('./lib/debug');
-
 var initialValue = 'Line1\nLine2\nLine3';
-var mdEditor= createMdEditor({ val: initialValue }); 
-var editor= createEditor({ val: initialValue }); 
 
-window.editor = editor;
-editor
-  .on('bold', function () {
-    var ed = editor.editor;
-    var range = ed.getSelectionRange()
-      , start  = util.normalizeLocation(range.start)
-      , end    = util.normalizeLocation(range.end)
-    ;
-    mdEditor.insertMark(start, end, '**', true)
+var markdown = require('./lib/markdown')({ val: initialValue });
+require('./lib/markdown/editor')({ 
+    markdown : markdown
+  , id       : 'markdown-editor'
+  , val     : initialValue
+});
+
+var wysiwyg = require('./lib/wysiwyg')({ val: initialValue });
+var wysiwygEditor = require('./lib/wysiwyg/editor')({ 
+    wysiwyg : wysiwyg
+  , id      : 'wysiwyg-editor'
+  , val     : initialValue
+}); 
+
+
+wysiwyg
+  .on('bold', function (e) {
+    markdown.insertMark(e.start, e.end, '**', true)
   })
-  .editor
-  .on('change', function (e) {
-    var action = e.data.action
-      , start  = util.normalizeLocation(e.data.range.start)
-      , end    = util.normalizeLocation(e.data.range.end)
-    ;
-
-    switch (action) {
-      case 'removeText':
-      case 'removeLines':
-        mdEditor.remove(start, end);
-        break;
-      case 'insertText':
-        mdEditor.insert(start, end, e.data.text);
-        break;
-      case 'insertLines':
-        mdEditor.insert(start, end, e.data.lines.join('\n'));
-        break;
-      default: 
-        console.log('unknown action', action);
-        break;
-    }
+  .on('remove', function (e) {
+    markdown.remove(e.start, e.end);
+  })
+  .on('insert', function (e) {
+    markdown.insert(e.start, e.end, e.text);
   });
