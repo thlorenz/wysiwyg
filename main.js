03f9724528af3b051f2909716d7e5abd4793a5a6
Thorsten Lorenz <thlorenz@gmx.de>
2014-02-05 18:00:44 -0500

    indicators
    - necessitated minor change to finding start marks (end right next to a
      start does not make it invalid anymore)

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 var initialValue = 'Line1\nLine2\nLine3';
 var renderer = require('./lib/markdown/renderer');
+var common = require('./lib/common');
 
 var markdown = require('./lib/markdown')({ raw: initialValue });
 require('./lib/markdown/editor')({ 
@@ -16,6 +17,15 @@ var wysiwygEditor = require('./lib/wysiwyg/editor')({
   , id      : 'wysiwyg-editor'
   , val     : initialValue
 }); 
+
+var $indicators = document.getElementById('style-indicators');
+var indicators = [
+    'bold'
+  , 'italic'
+  ].reduce(function (acc, type) {
+      acc[type] = $indicators.getElementsByClassName(type)[0]
+      return acc;
+  }, {})
 
 
 wysiwyg
@@ -32,9 +42,25 @@ wysiwyg
     markdown.insert(e.start, e.end, e.text);
   });
 
+wysiwygEditor.editor
+  .on('changeSelection', function () {
+    var cursorPos = wysiwygEditor.editor.getCursorPosition();
+    markdown.updateCursor(common.normalizeLocation(cursorPos));
+  });
+
 markdown
   .on('value-changed', function (e) {
     var rendered = renderer.render(e.val);
     var html = rendered.slice('<p>'.length, -('</p>'.length + 1))
     wysiwyg.render(html);
+  })
+  .on('active-marks-changed', function (e) {
+    Object.keys(indicators).forEach(function (k) {
+      var indicator = indicators[k];
+      if (indicator) indicator.classList.remove('active');
+    })
+    e.marks.forEach(function (m) {
+      var indicator = indicators[m.type];
+      if (indicator) indicator.classList.add('active');
+    });
   })
